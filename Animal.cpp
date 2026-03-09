427006b78121d8f863b6c19beb6eabde985f6e08
Naaabill <nabil.khouani@gmail.com>
2026-03-09 10:44:25 +0100

    pierre loup

---
+++ Animal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Animal.h"
+#include "stdlib.h"
+
+Animal::Animal(int maxX, int maxY){
+
+}
+Animal::Animal(int maxX, int maxY, int a, int b){
+
+}
+
+// Getters & Setters
+std::string Animal::getNom() const{
+
+}
+int Animal::getX() const{
+
+}
+int Animal::getY() const{
+
+}
+bool Animal::getVivant() const{
+
+}
+
+void Animal::setVivant(bool v){
+
+}
+// Attaque getAttaque() const{
+
+// }
+// // Méthodes métier
+// bool attaque(Animal &a){
+
+// }
